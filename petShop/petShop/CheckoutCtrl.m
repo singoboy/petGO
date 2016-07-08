a548c9ab012436c88c53f7d39787416a17d251dd
GaryFan <Gary@GaryFande-MacBook-Pro.local>
2016-07-08 11:07:06 +0800

    end of ui checkoutCtrl

--- petShop/petShop/CheckoutCtrl.m
+++ petShop/petShop/CheckoutCtrl.m
@@ -8,8 +8,12 @@
 
 #import "CheckoutCtrl.h"
 #import "CoreDataHelper.h"
+#import "Product.h"
 
 @interface CheckoutCtrl ()
+{
+    NSMutableArray *productList;
+}
 @property (weak, nonatomic) IBOutlet UILabel *memberNameLabel;
 @property (weak, nonatomic) IBOutlet UITextField *addressTextField;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
@@ -17,14 +21,56 @@
 @end
 
 @implementation CheckoutCtrl
-- (IBAction)payAction:(id)sender {
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        productList = [NSMutableArray array];
+        //query from coredata
+        NSManagedObjectContext *context = [CoreDataHelper sharedInstance].managedObjectContext;
+        //要抓取的物件
+        NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Product"];
+        //排序條件
+        NSSortDescriptor *timeSort = [NSSortDescriptor sortDescriptorWithKey:@"insertTime" ascending:YES];
+        //set 條件
+        [request setSortDescriptors:@[timeSort]];
+        NSError *error = nil ;
+        //執行查詢後的結果
+        NSArray *results = [context executeFetchRequest:request error:&error];
+        if (error) {
+            NSLog(@"error %@",error);
+        }
+        else{
+            [productList addObjectsFromArray:results];
+            NSLog(@"productList_count= %lu",(unsigned long)productList.count);
+        }
+    }
+    return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self getTotalPrice];
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)payAction:(id)sender {
+}
+
+
+
+
+-(void)getTotalPrice{
+    NSInteger totalPrice  = 0;
+    
+    for (Product* element in productList) {
+        totalPrice += [element.price integerValue];
+    }
+    
+    _totalLabel.text=[NSString stringWithFormat:@"總共 %ld 元" ,(long)totalPrice];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
