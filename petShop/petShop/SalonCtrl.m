c5658c0fd58d27ca548236541980bfad44ed7727
user12 <user12@admin12.local>
2016-07-14 10:32:06 +0800

    add salonCtrl datasource

--- petShop/petShop/SalonCtrl.m
+++ petShop/petShop/SalonCtrl.m
@@ -8,15 +8,65 @@
 
 #import "SalonCtrl.h"
 
-@interface SalonCtrl ()
+@interface SalonCtrl ()<UITableViewDelegate,UITableViewDataSource>
+{
+    NSArray *reserveArray ;
+}
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation SalonCtrl
 
+-(void)viewWillAppear:(BOOL)animated{
+    NSInteger memberID = 1 ;
+    NSString *urlStr = [NSString stringWithFormat:@"http://localhost:8888/petShop/reserve_json.php"];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
+                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                       timeoutInterval:60.0];
+    [request setHTTPMethod:@"POST"];
+    NSString *postString = [NSString stringWithFormat:@"memberID=%ld",(long)memberID ];
+    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    
+    NSError *error = nil;
+    reserveArray = [NSJSONSerialization JSONObjectWithData:response options:kNilOptions error:&error];
+    
+    if (error != nil) {
+        NSLog(@"Error parsing JSON.");
+    }
+        [self.tableView reloadData];
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource=self ;
+    self.tableView.delegate =self;
+    
     // Do any additional setup after loading the view.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return reserveArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reserveCell" forIndexPath:indexPath];
+    
+    NSDictionary *item = reserveArray[indexPath.row];
+    
+    cell.textLabel.text =  [item objectForKey: @"R_TIME"];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"會員ID:%@",[[item objectForKey: @"R_MEMBER_ID"] stringValue]];
+    
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
