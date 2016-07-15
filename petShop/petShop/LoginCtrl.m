a266bcc0695112963c772c9bc8f2c586d6950023
user12 <user12@admin12.local>
2016-07-15 16:31:50 +0800

    add alitile

--- petShop/petShop/LoginCtrl.m
+++ petShop/petShop/LoginCtrl.m
@@ -8,20 +8,66 @@
 
 #import "LoginCtrl.h"
 
-@interface LoginCtrl ()
+@interface LoginCtrl ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *accountText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordText;
 
 @end
 
 @implementation LoginCtrl
+- (IBAction)login:(id)sender {
+    
+    NSString * checkString =  [self checkInput];
+    if (checkString.length>0) {
+        [self showUIAlertCtrl:checkString];
+        return ;
+    }
+    
+    
+    
+    
+}
 
+-(NSString *)checkInput{
+    NSString *errString = @"";
+    if (self.accountText.text.length == 0) {
+        errString = [errString stringByAppendingString:@"請輸入帳號,"];
+    }
+    if (self.passwordText.text.length == 0) {
+        errString = [errString stringByAppendingString:@"請輸入密碼"];
+    }
+    
+    return errString;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.accountText.delegate =self ;
+    self.passwordText.delegate = self ;
     // Do any additional setup after loading the view.
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    [textField resignFirstResponder];
+    return YES ;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)showUIAlertCtrl:(NSString *)message{
+    
+    UIAlertController *alertController =
+    [UIAlertController alertControllerWithTitle:@"提示" message:message
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *alertAction =
+    [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:alertAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+    
 }
 
 /*
