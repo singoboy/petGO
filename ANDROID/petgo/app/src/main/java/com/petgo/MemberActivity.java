2cee88f17aa5667a58e245e70f0194cafb9b29e0
user12 <user12@admin12.local>
2016-08-17 14:47:08 +0800

    Android add getTotal

--- ANDROID/petgo/app/src/main/java/com/petgo/MemberActivity.java
+++ ANDROID/petgo/app/src/main/java/com/petgo/MemberActivity.java
@@ -7,8 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.petgo.common.Common;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import static  com.petgo.common.Common.*;
 
@@ -33,6 +44,7 @@ public class MemberActivity extends AppCompatActivity {
         super.onStart();
         if (Common.member !=null){
             tvName.setText(Common.member.getMemberName());
+            getTotal();
         }
 //        tvName.setText("您好");
 //        tvTotalPrice.setText("2000");
@@ -48,6 +60,7 @@ public class MemberActivity extends AppCompatActivity {
     public void logout(View view){
         Common.member = null;
         tvName.setText("");
+        tvTotalPrice.setText("");
         
     }
 
@@ -62,9 +75,45 @@ public class MemberActivity extends AppCompatActivity {
 
         if (requestCode ==  REQUEST_LOGIN && resultCode == Activity.RESULT_OK){
             //登入完成後
-            tvName.setText(Common.member.getMemberName());
+           // tvName.setText(Common.member.getMemberName());
         }
 
+    }
+
+    private void getTotal(){
+        String url =Common.URL+"order_getTotal.php";
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+                .add("memberID", String.valueOf(Common.member.getMemberID()))
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                Toast.makeText(getApplicationContext(), "累積金額讀取失敗", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String str = response.body().string();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvTotalPrice.setText(str);
+                        }
+                    });
+
+            }
+
+        });
 
     }
 
